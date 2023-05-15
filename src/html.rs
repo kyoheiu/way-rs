18336b4c4cba1d42f555ed59c9161ca82a12121b
Kyohei Uto <im@kyoheiu.dev>
2023-05-15 09:51:04 +0900

    Add style tag

--- src/html.rs
+++ src/html.rs
@@ -5,10 +5,48 @@ pub const INDEX: &str = r#"
 <head>
     <meta charSet="UTF-8" />
     <title>way</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
+        body {
+             background: rgb(238,174,202);
+        background: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%); 
+        }
+        
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 30px;
+        }
+        
+        input {
+            border: 0px;
+            border-radius: 5px;
+            margin: 5px;
+        }
+        
+        button {
+            color: #fff;
+            background-color: #666;
+            border: 1px solid #666;
+            border-radius: 5px;
+        }
+        
+        .welcome {
+            font-family: 'Bebas Neue', sans-serif;
+            font-size: 10em;
+            color: #666;
+            text-align: center;
+        }
+        
+        .logout-button {
+            text-align: center;
+        }
+    </style>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <link rel="icon"
         href="data:image/svg+xml,&lt;svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>&lt;text y=%22.9em%22 font-size=%2290%22>🤘&lt;/text>&lt;/svg>" />
-    <link rel="stylesheet" href="globals.css" />
     <script defer>
         const params = new URLSearchParams(document.location.search.substring(1));
         const ref = params.get("ref");
@@ -26,7 +64,7 @@ pub const INDEX: &str = r#"
 
     <body>
         <p>
-        <div class="welcome">suica</div>
+        <div class="welcome">way</div>
         </p>
         <div>
             <form id="login-form" method="post" action="api/login">
@@ -54,10 +92,48 @@ pub const VERIFIED: &str = r#"
 <head>
     <meta charSet="UTF-8" />
     <title>suica</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+
+        body {
+             background: rgb(238,174,202);
+        background: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%); 
+        }
+        
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 30px;
+        }
+        
+        input {
+            border: 0px;
+            border-radius: 5px;
+            margin: 5px;
+        }
+        
+        button {
+            color: #fff;
+            background-color: #666;
+            border: 1px solid #666;
+            border-radius: 5px;
+        }
+        
+        .welcome {
+            font-family: 'Bebas Neue', sans-serif;
+            font-size: 10em;
+            color: #666;
+            text-align: center;
+        }
+        
+        .logout-button {
+            text-align: center;
+        }
+    </style>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <link rel="icon"
         href="data:image/svg+xml,&lt;svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>&lt;text y=%22.9em%22 font-size=%2290%22>🤘&lt;/text>&lt;/svg>" />
-    <link rel="stylesheet" href="globals.css" />
 </head>
 
 <body>
